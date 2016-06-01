73ecec6f967ab2e8438080fac9194d8c5c29142c
Daisuke Fujita <dtanshi45@gmail.com>
2016-06-01 16:57:42 +0900

    Get articles

--- main.go
+++ main.go
@@ -1,16 +1,37 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
+type Article struct {
+	gorm.Model
+	Title  string `json:"title"`
+	Author string `json:"author"`
+	Body   string `json:"body"`
+}
+
 func main() {
+	db, err := gorm.Open("postgres", "user=postgres password=password host=localhost port=5432 dbname=gin_gorm_diary sslmode=disable")
+	if err != nil {
+		log.Fatal("Failed to connect database!")
+	}
+
+	db.AutoMigrate(&Article{})
+
 	r := gin.Default()
 
-	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello")
+	r.GET("/articles", func(c *gin.Context) {
+		var articles []Article
+
+		db.Select("title, author, body").Find(&articles)
+
+		c.JSON(http.StatusOK, articles)
 	})
 
 	r.Run()
